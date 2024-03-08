56b247a64067c2c8f90ffff979faabc32130cf23
boneysdharan <118093595+boneysdharan@users.noreply.github.com>
2024-03-08 10:14:28 +0530

    Create index.js

---
+++ src/data/index.js
@@ -0,0 +1,64 @@
+const customersData = [
+  {
+    id: 1,
+    firstName: 'Susan',
+    lastName: 'Jordon',
+    email: 'susan@example.com',
+    balance: '95000',
+    add: '0',
+    deduct: '0',
+    updated_balance:'95000',
+    date: '2019-04-11',
+    date_balance_change: '2019-04-11'
+  },
+  {
+    id: 2,
+    firstName: 'Adrienne',
+    lastName: 'Doak',
+    email: 'adrienne@example.com',
+    balance: '80000',
+    add: '0',
+    deduct: '0',
+    updated_balance:'80000',
+    date: '2019-04-17',
+    date_balance_change: '2019-04-11'
+  },
+  {
+    id: 3,
+    firstName: 'Rolf',
+    lastName: 'Hegdal',
+    email: 'rolf@example.com',
+    balance: '79000',
+    add: '0',
+    deduct: '0',
+    updated_balance:'79000',
+    date: '2019-05-01',
+    date_balance_change: '2019-04-11'
+  },
+  {
+    id: 4,
+    firstName: 'Kent',
+    lastName: 'Rosner',
+    email: 'kent@example.com',
+    balance: '56000',
+    add: '0',
+    deduct: '0',
+    updated_balance:'56000',
+    date: '2019-05-03',
+    date_balance_change: '2019-04-11'
+  },
+  {
+    id: 5,
+    firstName: 'Arsenio',
+    lastName: 'Grant',
+    email: 'arsenio@example.com',
+    balance: '65000',
+    add: '0',
+    deduct: '0',
+    updated_balance:'65000',
+    date: '2019-06-13',
+    date_balance_change: '2019-04-11'
+  },
+];
+
+export { customersData };
